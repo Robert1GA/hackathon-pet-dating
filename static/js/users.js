fe41f154c6c13469c60c24a7588d766dce91b5d2
Ryne Schillinger <ryneschillinger@gmail.com>
2017-01-26 17:29:28 -0800

    created users.js file

---
+++ static/js/users.js
@@ -0,0 +1,202 @@
+users = [
+	{
+		username: "Dityzxrm",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "BrilliantBabe",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "Lexecenti",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "Cuddlysist",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "MonWanna",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "CrazyCatLady",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "NicerChik",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "Boomion",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "BeastBeastBurnt",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "HotSpecial",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "Amasishi",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+	{
+		username: "Measusa",
+		age: 30,
+		gender: "female",
+		photo: "woman1.jpg",
+		pets: {
+			cats: [
+				{name: "Fluffy", img: ""},
+				{name: "Pip", img: ""},
+				{name: "Fester", img: ""}
+			],
+			dogs: [
+				{name: "Nacho", img: ""}
+				]
+		},
+	},
+]
+
+console.log(users[0].username);
+console.log(users[0].age);
+console.log(users[0].gender);
+console.log(users[0].photo);
+console.log("Cats: " + users[0].pets.cats.length);
+console.log("Dogs: " + users[0].pets.dogs.length);
+console.log(users[0].pets.cats[0].name);
